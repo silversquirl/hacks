6baba7a3819a5b063d55b551fe2ba5901a9f61ac
Samadi van Koten <me@vktec.co.uk>
2017-02-16 16:37:58 +0000

    Create a polyglot

---
+++ polyglot.tcl.sh.py.c
@@ -0,0 +1,22 @@
+#if 0
+# A polyglot written in Tcl, Python, shell script and C (compiles with 1 warning using GCC)
+#\
+"true""""\
+"
+#\
+puts() { echo "$@"; }
+puts "Hello, world!"
+#"""; print("Hello, world!"); """
+exit
+#endif
+
+#include <stdio.h>
+int main() {
+    puts("Hello, world!");
+    return 0;
+}
+
+#if 0
+"""
+#endif
+
