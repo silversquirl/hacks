c599b8c910fd417509b0ded08942b30e0dbd215f
Samadi van Koten <me@vktec.co.uk>
2017-02-16 16:38:07 +0000

    Create a quine

---
+++ quine.c
@@ -0,0 +1,2 @@
+int printf(const char *format, ...); int main() { char *text; printf(text="int printf(const char *format, ...); int main() { char *text; printf(text=%c%s%c, 0x22, text, 0x22, 0x0A); return 0; }%c", 0x22, text, 0x22, 0x0A); return 0; }
+
